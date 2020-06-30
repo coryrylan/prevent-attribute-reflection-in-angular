d0127c19b2b560aefdf5cea60d8cad1ebcb4d1b6
coryrylan <splintercode.cb@gmail.com>
2020-06-29 23:08:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-szsw3k
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-szsw3k)
---
+++ src/app/switch/switch.component.ts
@@ -0,0 +1,65 @@
+import { Component, Input, forwardRef, HostBinding } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-switch',
+  templateUrl: './switch.component.html',
+  styleUrls: ['./switch.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SwitchComponent),
+      multi: true
+    }
+  ]
+})
+export class SwitchComponent implements ControlValueAccessor {
+  @HostBinding('attr.id')
+  externalId = '';
+
+  @Input()
+  set id(value: string) {
+    this._ID = value;
+    this.externalId = null;
+  }
+
+  get id() {
+    return this._ID;
+  }
+
+  private _ID = '';
+
+  @Input('value') _value = false;
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  get value() {
+    return this._value;
+  }
+
+  set value(val) {
+    this._value = val;
+    this.onChange(val);
+    this.onTouched();
+  }
+
+  constructor() {}
+
+  registerOnChange(fn) {
+    this.onChange = fn;
+  }
+
+  writeValue(value) {
+    if (value) {
+      this.value = value;
+    }
+  }
+
+  registerOnTouched(fn) {
+    this.onTouched = fn;
+  }
+
+  switch() {
+    this.value = !this.value;
+  }
+}
